67af6eba71a97a07d26ce53ae711a00d6c33db38
Griffin Ledingham <gcledingham@gmail.com>
2019-08-01 09:06:29 -0700

    Commenting/readme improvement

--- ASDecoder.php
+++ ASDecoder.php
@@ -7,13 +7,29 @@ use AppleSignIn\Vendor\JWT;
 
 use Exception;
 
+/**
+ * Decode Sign In with Apple identity token, and produce an ASPayload for
+ * utilizing in backend auth flows to verify validity of provided user creds.
+ */
 class ASDecoder {
+    /**
+     * Parse a provided Sign In with Apple identity token.
+     *
+     * @param string $identityToken
+     * @return object|null
+     */
     public static function getAppleSignInPayload(string $identityToken) : ?object
     {
         $identityPayload = self::decodeIdentityToken($identityToken);
         return new ASPayload($identityPayload);
     }
 
+    /**
+     * Decode the Apple encoded JWT using Apple's public key for the signing.
+     *
+     * @param string $identityToken
+     * @return object
+     */
     public static function decodeIdentityToken(string $identityToken) : object {
         $publicKeyData = self::fetchPublicKey();
 
@@ -25,6 +41,12 @@ class ASDecoder {
         return $payload;
     }
 
+    /**
+     * Fetch Apple's public key from the auth/keys REST API to use to decode
+     * the Sign In JWT.
+     *
+     * @return array
+     */
     public static function fetchPublicKey() : array {
         $publicKeys = file_get_contents('https://appleid.apple.com/auth/keys');
         $decodedPublicKeys = json_decode($publicKeys, true);
@@ -48,6 +70,10 @@ class ASDecoder {
     }
 }
 
+/**
+ * A class decorator for the Sign In with Apple payload produced by
+ * decoding the signed JWT from a client.
+ */
 class ASPayload {
     protected $_instance;
 
--- README.md
+++ README.md
@@ -10,3 +10,28 @@ Use composer to manage your dependencies and download php-apple-signin:
 ```bash
 composer require griffinledingham/php-apple-signin
 ```
+
+Example
+-------
+```php
+<?php
+use AppleSignIn\ASDecoder;
+
+$clientUser = "example_client_user";
+$identityToken = "example_encoded_jwt";
+
+$appleSignInPayload = ASDecoder::getAppleSignInPayload($identityToken);
+
+/**
+ * Obtain the Sign In with Apple email and user creds.
+ */
+$email = $appleSignInPayload->getEmail();
+$user = $appleSignInPayload->getUser();
+
+/**
+ * Determine whether the client-provided user is valid
+ */
+$isValid = $appleSignInPayload->verifyUser($clientUser);
+
+?>
+```
