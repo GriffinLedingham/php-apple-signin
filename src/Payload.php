34214b003843abff951b57137b74ed1d272aa484
Phil M <pmuir@deputy.com>
2020-06-16 09:52:35 +1000

    Delete magic methods off Payload
    They are not used or needed here. We are decorating the underlying
    decoded JWT stdClass/Object and wish to provide getters.

--- src/Payload.php
+++ src/Payload.php
@@ -2,7 +2,6 @@
 
 namespace AppleSignIn;
 
-use Exception;
 
 /**
  * A class decorator for the Sign In with Apple payload produced by
@@ -23,27 +22,8 @@ class Payload
         if (is_null($instance)) {
             throw new Exception('ASPayload received null instance.');
         }
+
         $this->_instance = $instance;
-    }
-
-    public function __call($method, $args)
-    {
-        return call_user_func_array(array($this->_instance, $method), $args);
-    }
-
-    public function __get($key)
-    {
-        return $this->_instance->$key ?? null;
-    }
-
-    public function __set($key, $val)
-    {
-        return $this->_instance->$key = $val;
-    }
-
-    public function __isset($name)
-    {
-        return isset($this->_instance->$name);
     }
 
     public function getEmail(): ?string
