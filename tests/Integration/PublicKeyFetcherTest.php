5865f7a943c8225d63527a9be025f77548cb06f6
Phil M <pmuir@deputy.com>
2020-06-16 08:42:46 +1000

    Add (failing) tests for src/PublicKeyFetcher.php

---
+++ tests/Integration/PublicKeyFetcherTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppleSignInTest\Integration;
+
+use AppleSignIn\Http\Curl;
+use AppleSignIn\PublicKeyFetcher;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class PublicKeyFetcherTest
+ * @package AppleSignInTest\Integration
+ */
+class PublicKeyFetcherTest extends TestCase
+{
+    /** @var string */
+    protected const KID = '86D88Kf';
+
+    /** @var PublicKeyFetcher */
+    private $fetcher;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->fetcher = new PublicKeyFetcher(new Curl());
+    }
+
+    public function testFetchRetrievesKeyWithWellKnownKid(): void
+    {
+        $result = $this->fetcher->fetch(self::KID);
+
+        $this->assertArrayHasKey('publicKey', $result);
+        $this->assertArrayHasKey('alg', $result);
+    }
+
+    /**
+     * @expectedException \AppleSignIn\Exception
+     */
+    public function testFetchThrowsExceptionWithInvalidKid(): void
+    {
+        $result = $this->fetcher->fetch('lol');
+
+        print_r($result);
+    }
+}
