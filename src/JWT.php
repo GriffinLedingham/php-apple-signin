acaeefb2bfbce35f8f50f1c298633446b6f498b4
Phil M <pmuir@deputy.com>
2020-06-16 11:19:06 +1000

    Add unit test for \AppleSignInTest\Unit\JWTTest::testGetPublicKeyKid

--- src/JWT.php
+++ src/JWT.php
@@ -13,10 +13,10 @@ class JWT extends \Firebase\JWT\JWT
     public static function getPublicKeyKid($jwt)
     {
         $tks = explode('.', $jwt);
-        if (count($tks) != 3) {
+        if (count($tks) !== 3) {
             throw new UnexpectedValueException('Wrong number of segments');
         }
-        list($headb64, $bodyb64, $cryptob64) = $tks;
+        [$headb64, $bodyb64, $cryptob64] = $tks;
         if (null === ($header = static::jsonDecode(static::urlsafeB64Decode($headb64)))) {
             throw new UnexpectedValueException('Invalid header encoding');
         }
---
+++ tests/Unit/JWTTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppleSignInTest\Unit;
+
+
+use AppleSignIn\JWT;
+use PHPUnit\Framework\TestCase;
+
+class JWTTest extends TestCase
+{
+
+    public function testGetPublicKeyKid()
+    {
+        $claims = [
+            "alg" => "ES256",
+            "kid" => "TESTKID",
+            "typ" => "JWT"
+        ];
+        $token = JWT::urlsafeB64Encode(JWT::jsonEncode($claims)) . '.xxx.xxx';
+
+        $result = JWT::getPublicKeyKid($token);
+
+        $this->assertEquals('TESTKID', $result);
+    }
+
+}
